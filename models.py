a67ebee33fb6ca857beaef7d85b63a489f3b4a41
Daniel <dm5022001@gmail.com>
2025-02-02 12:11:24 +0400

    ...

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, TIMESTAMP, text
 from database import Base
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer,primary_key=True)
-    name = Column(String,nullable=False)
-    email = Column(String,nullable=False)
-    password = Column(String,nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    token = Column(Integer, nullable=False, server_default='10')
+    time = Column(TIMESTAMP, nullable=False, server_default=text("NOW() + INTERVAL '10 days'"))
